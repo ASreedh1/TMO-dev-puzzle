0ed16de3ca7e490e8121735d8e59e9e133f7603a
Ajith Sreedharan <Ajith.Sreedharan1@T-Mobile.com>
2020-01-20 18:32:44 +0530

    Task 1: naming functions and properties

--- libs/util/src/lib/google-chart/google-chart.util.ts
+++ libs/util/src/lib/google-chart/google-chart.util.ts
@@ -1,7 +1,7 @@
 import { googleChartConstant } from './google-chart.constant';
 import { GoogleChart } from './google-chart.type';
 
-export function handleChartOptions(chartOptions?: GoogleChart): GoogleChart {
+export function overrideChartOptions(chartOptions?: GoogleChart): GoogleChart {
   return {
     title: (chartOptions && chartOptions.title) || googleChartConstant.TITLE,
     type: (chartOptions && chartOptions.type) || googleChartConstant.TYPE,
